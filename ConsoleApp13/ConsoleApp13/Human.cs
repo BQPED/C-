08d3941569cca491f7b8c713256b18afe6ca9dd3
BQPED <zakdan07@mail.ru>
2026-04-14 09:18:41 +0300

    Классы как поля
    Классы как поля

---
+++ ConsoleApp13/ConsoleApp13/Human.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Human
+    {
+        private string lastname;
+        private string name;
+        private string secondname;
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+        }
+
+        public Human(string lastname, string name, string secondname, int age)
+        {
+            this.lastname = lastname;
+            this.name = name;
+            this.secondname = secondname;
+            this.age = age;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine(lastname + " " + name + " " + secondname + ", " + age + Option());
+        }
+
+        private string Option()
+        {
+            string s;
+            if (age > 10 && age <= 20)
+                s = " лет";
+            else
+            {
+                switch (age % 10)
+                {
+                    case 1:
+                        s = " год";
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        s = " года";
+                        break;
+                    default:
+                        s = " лет";
+                        break;
+                }
+            }
+            return s;
+        }
+
+        public static Human operator !(Human a)
+        {
+            return new Human(a.lastname, a.name, a.secondname, a.age + 1);
+        }
+
+        public static Human operator /(Human a, string s)
+        {
+            return new Human(s, a.name, a.secondname, a.age);
+        }
+    }
+}
---
+++ ConsoleApp13/ConsoleApp13/Office.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Office
+    {
+        private Human boss;
+        private string name;
+        private int countworker;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public int CountWorker
+        {
+            set { countworker = value; }
+            get { return countworker; }
+        }
+
+        public Human Boss
+        {
+            set { boss = value; }
+            get { return boss; }
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Подразделение: {0}, работающих {1} человек", name, countworker);
+            if (boss != null)
+                boss.Info();
+        }
+
+        public Office()
+        {
+        }
+
+        public Office(string lastname, string name, string secondname, int age, string name1, int countworker)
+        {
+            this.name = name1;
+            this.countworker = countworker;
+            boss = new Human(lastname, name, secondname, age);
+        }
+    }
+}
